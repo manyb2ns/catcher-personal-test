d021c559a3172ae89e3f13febc58b314a3bc5e05
manyb2ns <manby2ns@gmail.com>
2023-10-15 16:19:54 +0900

    add nat

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -82,3 +82,20 @@ resource "aws_route_table_association" "private_rt_asso" {
   subnet_id = aws_subnet.private_subnets[count.index].id
   route_table_id = aws_route_table.private_rt.id
 }
+
+resource "aws_eip" "nat_ip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_ip.id
+  subnet_id     = aws_subnet.private_subnets[0].id
+
+  tags = {
+    Name = "${var.project}-${var.environment}-nat"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.igw]
+}
